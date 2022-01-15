f48a621bd0a4179c83109fb6add1964ce209d8e2
Luis Viegas <luisviegas@MacBook-Pro-de-Luis-2.local>
2022-01-15 13:58:05 +0000

    final

--- resources/views/pages/mainPage.blade.php
+++ resources/views/pages/mainPage.blade.php
@@ -5,6 +5,9 @@
 @section('content')
 
 <section class="timeline" id="public timeline">
+  @if (Auth::check())
+  @include('partials.new_post')
+  @endif
   @foreach ($posts as $post )
     @if($post->poster->is_private==false)
     @include('partials.post', ['post'=> $post])
