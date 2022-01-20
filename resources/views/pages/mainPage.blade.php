a0a245cde16733b8faab0f3a6ab6c6121107aa8e
Pedro Lima <up2010605125@up.pt>
2022-01-20 00:52:46 +0000

    Main Timeline shows all posts the user has access to.

--- resources/views/pages/mainPage.blade.php
+++ resources/views/pages/mainPage.blade.php
@@ -9,7 +9,7 @@
     @include('partials.new_post')
   @endif
   @foreach ($posts as $post )
-    @if($post->poster->is_private==false)
+    @if($post->poster->is_private==false || (Auth::check() && ( Auth::user()->is_admin || $post->user_id == Auth::id() || Auth::user()->isFriend($post->poster_id))) )
       @include('partials.post', ['post'=> $post])
     @endif
   @endforeach
