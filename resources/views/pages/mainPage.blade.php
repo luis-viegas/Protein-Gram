367cf231d79c069e17ae5092765d6fef1453cf3f
Luis Carlos Barros Viegas <up201904979@fe.up.pt>
2022-01-20 01:04:03 +0000

    Revert "Main Timeline shows all posts the user has access to."
    This reverts commit a0a245cde16733b8faab0f3a6ab6c6121107aa8e

--- resources/views/pages/mainPage.blade.php
+++ resources/views/pages/mainPage.blade.php
@@ -9,7 +9,7 @@
     @include('partials.new_post')
   @endif
   @foreach ($posts as $post )
-    @if($post->poster->is_private==false || (Auth::check() && ( Auth::user()->is_admin || $post->user_id == Auth::id() || Auth::user()->isFriend($post->poster_id))) )
+    @if($post->poster->is_private==false)
       @include('partials.post', ['post'=> $post])
     @endif
   @endforeach
