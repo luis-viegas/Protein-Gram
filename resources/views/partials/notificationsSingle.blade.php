edfd140ce2d297d0125bb426c941c758e60f4919
Pedro Lima <up2010605125@up.pt>
2022-01-20 20:12:35 +0000

    Notifications now come with an id attribute

--- resources/views/partials/notificationsSingle.blade.php
+++ resources/views/partials/notificationsSingle.blade.php
@@ -1,22 +1,22 @@
 @if($notification)
     @switch($notification->type)
         @case('comment')
-            <div class="notification">{{$notification->name}} has commented on your post.</div>
+            <div class="notification" notification_id="{{$notification->id}}">{{$notification->name}} has commented on your post.</div>
         @break
         @case('post_like')
-            <div class="notification">{{$notification->name}} has liked your post.</div>
+            <div class="notification" notification_id="{{$notification->id}}">{{$notification->name}} has liked your post.</div>
         @break
         @case('comment_like')
-            <div class="notification">{{$notification->name}} has liked your comment.</div>
+            <div class="notification" notification_id="{{$notification->id}}">{{$notification->name}} has liked your comment.</div>
         @break
         @case('comment_tag')
-            <div class="notification">{{$notification->name}} has tagged you in a comment;</div>
+            <div class="notification" notification_id="{{$notification->id}}">{{$notification->name}} has tagged you in a comment;</div>
         @break
         @case('message')
-            <div class="notification">{{$notification->name}} has messaged you.</div>
+            <div class="notification" notification_id="{{$notification->id}}">{{$notification->name}} has messaged you.</div>
         @break
         @case('comment_reply')
-            <div class="notification">{{$notification->name}} has replied to your comment.</div>
+            <div class="notification" notification_id="{{$notification->id}}">{{$notification->name}} has replied to your comment.</div>
         @break
     @endswitch
 @endif
